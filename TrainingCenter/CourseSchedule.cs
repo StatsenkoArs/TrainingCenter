1950c0d085f710fc3ad2621f1a71fa12d611caf0
qquodd <ura.kvochkin@gmail.com>
2024-12-17 18:37:37 +0300

    da nu nahui

--- TrainingCenter/CourseSchedule.cs
+++ TrainingCenter/CourseSchedule.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace TrainingCenter
@@ -14,7 +8,29 @@ namespace TrainingCenter
     {
         public CourseSchedule()
         {
+            DataBase dataBase = new DataBase();
+
+            var dataTable = new DataTable();
+            string query = "SELECT \r\n    " +
+                "FORMAT(StartDate, 'dd.MM.yyyy') AS 'Дата начала',\r\n    " +
+                "T.FullName AS 'Преподаватель',\r\n    " +
+                "G.SeatsAvailable AS 'Количество мест',\r\n    " +
+                "CC.Cost AS 'Стоимость',\r\n    " +
+                "CC.TrainingType AS 'Тип обучения'\r\n" +
+                "FROM \r\n    " +
+                "GroupSchedules G\r\n" +
+                "JOIN \r\n    " +
+                "Teachers T ON G.TeacherId = T.Id\r\n" +
+                "JOIN \r\n    CourseCosts CC ON G.CourseCostId = CC.Id;\r\n";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.SelectCommand = new SqlCommand(query, dataBase.getConnection());
+            adapter.Fill(dataTable);
+
             InitializeComponent();
+
+            dataGridView1.DataSource = dataTable;
         }
     }
 }
---
+++ TrainingCenter/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingCenter
+{
+    internal class DataBase
+    {
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AFG55QJ\SQLEXPRESS;Initial Catalog=educationalСenter;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
